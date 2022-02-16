9e7908af31cb7747e6ace895c719bc0cd2906f9e
Magali Morin <magali.morin@student-cs.fr>
2022-02-16 15:45:14 +0100

    Adding folders and TP2

--- check_env.py
+++
@@ -1,9 +0,0 @@
-import gym
-import numpy as np
-import matplotlib
-import sys
-
-print("python : ", sys.version)
-print("numpy :", np.__version__)
-print("gym :", gym.__version__)
-print("matplotlib :", matplotlib.__version__)
