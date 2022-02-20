2da43072c15ac2a37177f355ba4f56622e9c8595
Magali Morin <magali.morin@student-cs.fr>
2022-02-20 16:29:23 +0100

    Adding descriptions to the graphs and converting into html

--- html.sh
+++ html.sh
@@ -1,7 +1,7 @@
 mkdir ./docs ./docs/td2
 
 # TD2
-jupyter nbconvert --to html ./TD2/TD2.ipynb
+python -m jupyter nbconvert --to html ./TD2/TD2.ipynb
 mv ./TD2/TD2.html ./docs/td2/index.html
-jupyter nbconvert --to html ./TD2/TD2_discrete.ipynb
+python -m jupyter nbconvert --to html ./TD2/TD2_discrete.ipynb
 mv ./TD2/TD2_discrete.html ./docs/td2/discrete.html
